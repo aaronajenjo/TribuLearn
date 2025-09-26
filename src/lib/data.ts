a509cf7247bb727e54447ca8e4902fabc25ec6c8
Aaron Ajenjo <aaronajenjomartin@gmail.com>
2025-09-26 09:09:24 +0000

    En el tab de intermediate deja el segundo resource pero con el link del

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -92,13 +92,8 @@ export const learningPaths = (t: Translator): Technology[] => [
             sopraResources: [
               {
                 type: "course",
-                title: "C# Intermediate Learning Track",
-                url: "https://soprasteria.percipio.com/track/3ca3af36-ed2c-49b4-92c6-7e02d7b80d22",
-              },
-              {
-                type: "course",
                 title: "Web Apps with C# & ASP.NET Track",
-                url: "https://soprasteria.percipio.com/track/fed7500e-f1c5-42cf-9b14-2c738633361a",
+                url: "https://soprasteria.percipio.com/track/3ca3af36-ed2c-49b4-92c6-7e02d7b80d22",
               },
             ],
             youtubeResources: [
