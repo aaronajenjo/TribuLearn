325e0499f55ace09793d45fa55c9866dc0febe24
Aaron Ajenjo <aaronajenjomartin@gmail.com>
2025-09-30 09:27:19 +0000

    empecemos por la pantalla de Refactor, haz que ocupe el 100%

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -95,7 +95,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
               <UserNav />
             </div>
           </header>
-          <main className="flex-1 overflow-y-auto p-4 sm:p-6 lg:p-8">
+          <main className="flex-1 overflow-y-auto p-4 sm:p-6">
             {children}
           </main>
         </div>
